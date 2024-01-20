2019790fe5c8eb0776887b54e2c023d458f121b4
bruno <brunocardamone09@gmail.com>
2024-01-20 17:08:39 -0300

    arreglando problema de estilos con el footer

--- src/context/useContext.jsx
+++ src/context/useContext.jsx
@@ -5,7 +5,7 @@ export default function UseState(params) {
   const [StateGlobal, setStateGlobal] = useState({
     des: "Hace casi un año comencé a practicar con React, la cantidad de herramientas que te otorga es increíble, sumado a su baja curva de aprendizaje hace que sea de lo más cómodo para trabajar.",
     level: 'intermediate',
-    img: "https://brunocarda2034.github.io/Bruno/assets/react-Fdl7Tblo.svg",
+    img: "https://brunocarda2005.github.io/Bruno/assets/react-Fdl7Tblo.svg ",
     alt: "react bruno cardamone skills"
   });
 
