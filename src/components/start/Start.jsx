edf026cd2e3dfbb6e536c6a3a4a9d1061504e76f
Bruno Cardamone <brunocardamone03@gmail.com>
2024-11-15 17:56:09 -0300

    feat: change start, add lamp and delete wallpeper

--- src/components/start/Start.jsx
+++ src/components/start/Start.jsx
@@ -1,43 +1,19 @@
 import StartAbout from "./components/about/StartAbout";
 import StartLink from "./components/links/StartLinks";
 import { StartTools } from "./components/tools/StartTools";
-import StartWallpaper from "./components/wallpaper/StartWallpaper";
+import StartSetup from "./components/setup/StartSetup";
 import "./Start.css";
 
 export default function Main() {
   return (
     <main className="main" id="home">
-      {/* <section className="main-me">
-        <div className="main-me-content">
-          <aside>
-            <h2 className="main-me-content-h">
-              Bruno <b>Cardamone</b>
-            </h2>
-            <p>Frontend developer</p>
-          </aside>
-          <section className="main-me-content-contact">
-            <a href="https://www.linkedin.com/in/bruno-cardamone-470032216/">
-              <FaLinkedin />
-            </a>
-            <a href="mailto:brunocardamone03@gmail.com">
-              <FaEnvelope />
-            </a>
-            <a href="https://github.com/Brunocarda2005">
-              <FaGithub />
-            </a>
-          </section>
-          <a className="main-me-content-a" download={"Curriculum"} href={cv}>
-            <span className="main-me-content-a-text">Dowload CV</span>
-          </a>
-        </div>
-      </section> */}
       <div className="blob position-start">
         <span className="blob-span"></span>
       </div>
       <section className="main__content-grid">
         <StartAbout />
         <StartTools />
-        <StartWallpaper />
+        <StartSetup />
         <StartLink />
       </section>
     </main>
---
+++ src/components/start/components/setup/StartSetup.jsx
@@ -0,0 +1,21 @@
+import "./StartWallpaper.css";
+import setupIMG from "../../../../static/svg/setup.svg";
+// import lightIMG from "../../../../static/svg/light.svg";
+import lampIMG from "../../../../static/svg/lamp.svg";
+
+export default function StartSetup() {
+  return (
+    <aside className="main__content-grid__container wallpaper box-shadow">
+      <section className="main__content-grid__container__lamp">
+        <picture>
+          <img src={lampIMG} alt="lamp light svg" className="light__img" />
+        </picture>
+      </section>
+      <section className="main__content-grid__container__setup">
+        <picture>
+          <img src={setupIMG} alt="setup svg" className="setup__img" />
+        </picture>
+      </section>
+    </aside>
+  );
+}
